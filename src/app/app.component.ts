dcc031e1d346965f4a6117fcda8aebef987a7056
pca1987 <pca1987@gmail.com>
2018-08-30 10:29:58 -0400

    adding firebase libraries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-firebase';
+
+
+  testDB(): void {
+    alert('test db');
+  }
 }
